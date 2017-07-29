0f3c1291d0ce57549a9b8aa6f4f00c8b8d3478dd
Spjutbjorn <spjutbjorn@gmail.com>
2017-07-29 22:31:55 +0200

    revert net core install script to ubuntu 16.04

--- Ubuntu/Install/DotNetCore.sh
+++ Ubuntu/Install/DotNetCore.sh
@@ -1,10 +1,7 @@
-
-
 #!/bin/sh
-# Install DotNet Core for ubuntu 16.10
+# Install DotNet Core for ubuntu 16.04
 
-
-sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 apt-get update
 apt-get install dotnet-dev-1.0.4
